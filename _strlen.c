9c46c5b91cea995fc7352a1a8ab3488e7dc9d0c4
Hemo5478 <hamza9638@hotmail.com>
2023-05-03 02:55:14 -0700

    Fixing

---
+++ _strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - A function that returns the length of a string
+ * @s: A string
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
