d2d960e1c4f1ec38220f99413795a6297cfa11d8
Hemo5478 <hamza9638@hotmail.com>
2023-05-03 02:56:14 -0700

    Fixing

---
+++ _printf.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+int _printf(const char* format, ...)
+{
+    char c;
+    int hemo;
+
+    va_list args;
+    va_start(args, format);
+
+    hemo = 0;
+
+    while (*format != '\0')
+    {
+        if (*format == '%')
+        {
+            format++;
+            if (*format == 'c')
+            {
+                c = (char)va_arg(args, int);
+                putchar(c);
+                hemo++;
+            }
+            else if (*format == 's')
+            {
+                const char* str = va_arg(args, const char*);
+                _print_str(str);
+                hemo += _strlen(str);
+            }
+            else if (*format == '%')
+            {
+                putchar('%');
+                hemo++;
+            }
+            else
+            {
+                putchar('%');
+                putchar(*format);
+                hemo += hemo + 2;
+            }
+        }
+        else
+        {
+            putchar(*format);
+            hemo++;
+        }
+
+        format++;
+    }
+
+    va_end(args);
+
+    return hemo;
+}
