f9be13dc18f7ed7933254ba43ea18c05b2e2d8e5
Hemo5478 <hamza9638@hotmail.com>
2023-05-03 03:21:21 -0700

    Fixing

--- _printf.c
+++ _printf.c
@@ -1,54 +1,53 @@
 #include "main.h"
 
-int _printf(const char* format, ...)
+int _printf(const char *format, ...)
 {
-    char c;
-    int hemo;
+	char c;
+	int hemo;
 
-    va_list args;
-    va_start(args, format);
+	va_list args;
+	va_start(args, format);
 
-    hemo = 0;
+	hemo = 0;
 
-    while (*format != '\0')
-    {
-        if (*format == '%')
-        {
-            format++;
-            if (*format == 'c')
-            {
-                c = (char)va_arg(args, int);
-                putchar(c);
-                hemo++;
-            }
-            else if (*format == 's')
-            {
-                const char* str = va_arg(args, const char*);
-                _print_str(str);
-                hemo += _strlen(str);
-            }
-            else if (*format == '%')
-            {
-                putchar('%');
-                hemo++;
-            }
-            else
-            {
-                putchar('%');
-                putchar(*format);
-                hemo += hemo + 2;
-            }
-        }
-        else
-        {
-            putchar(*format);
-            hemo++;
-        }
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				c = (char)va_arg(args, int);
+				putchar(c);
+				hemo++;
+			}
+			else if (*format == 's')
+			{
+				const char *str = va_arg(args, const char *);
+				_print_str(str);
+				hemo += _strlen(str);
+			}
+			else if (*format == '%')
+			{
+				putchar('%');
+				hemo++;
+			}
+			else
+			{
+				putchar('%');
+				putchar(*format);
+				hemo += hemo + 2;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			hemo++;
+		}
+		format++;
+	}
 
-        format++;
-    }
+	va_end(args);
 
-    va_end(args);
-
-    return hemo;
+	return (hemo);
 }
