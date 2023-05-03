508dba8e964a3db8538add1685ddc64227e14dcf
Hemo5478 <hamza9638@hotmail.com>
2023-05-03 02:37:47 -0700

    Fixing

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdarg.h>
+
+#endif
